4515109ed5e50cdab55e19dab79d8759d1a6b34e
Kryzyss <Apmyp@o2.pl>
2025-01-06 04:55:37 +0100

    Update package.json and server code for Railway

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
